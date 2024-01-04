b32329ba3455d2ef55be3b3a18b31b6668517b19
aaronkern07 <aaronkern07@gmail.com>
2024-01-03 21:21:34 -0600

    Added some new attributes and types for game test page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,31 @@
+type gameType = {
+  id: number,
+  name: string,
+  console_name: string,
+  price: string,
+  condition: string,
+  is_boxed: boolean
+}
 
 async function fetchGames() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/games`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/games`, { cache: 'no-store' })
   const data = await res.json()
-  console.log(data);
-  
   return data
 };
 
 export default async function Home() {
   const games = await fetchGames()
   return (
-    <main className="flex flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-12">
       <h1 className="text-4xl font-bold">Retro Game Store</h1>
       <div className="flex flex-wrap justify-center">
-        {games.map((game) => (
-          <div key={game.id} className="flex flex-col items-center justify-center m-4">
+        {games.map((game : gameType) => (
+          <div key={game.id} className="flex flex-col items-center justify-center m-4 border-white border-2 p-4 min-w-96">
             <h2 className="text-xl font-bold">{game.name}</h2>
-            <p className="text-lg">{game.price}</p>
+            <p className="text-lg">{game.console_name}</p>
+            <p className="text-md">{game.price}</p>
+            <p className="text-md">{game.condition} Condition</p>
+            <p className="text-md">{game.is_boxed ? "In Box": "Cartridge Only"}</p>
           </div>
         ))}
       </div>
